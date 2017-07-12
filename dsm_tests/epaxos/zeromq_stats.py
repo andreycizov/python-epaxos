a7dfe323e14dbe4905e1f36dd68c642d23435e30
Andrey Cizov <acizov@gmail.com>
2017-07-12 17:09:14 +0200

    All

--- dsm_tests/epaxos/zeromq_stats.py
+++ dsm_tests/epaxos/zeromq_stats.py
@@ -1,4 +1,4 @@
 import sys
 import pstats
-p = pstats.Stats('1.profile')
-p.sort_stats(sys.argv[1]).print_stats()
+p = pstats.Stats(sys.argv[1] + '.profile')
+p.sort_stats(sys.argv[2]).print_stats()
